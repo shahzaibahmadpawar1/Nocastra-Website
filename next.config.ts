50153066acc1a6e7e5d40235dbdb34fb4e18f4d9
shahzaibahmadpawar1 <shazi8402@gmail.com>
2026-07-16 12:17:31 +0500

    upadted .yml and next.config.ts file updated for npm

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export",
-  reactCompiler: true,
-  trailingSlash: true, // Creates clean folders for sub-pages on cPanel
-  images: {
-    unoptimized: true, // Disables the Node.js dynamic image optimizer
-  },
+const nextConfig = {
+  output: 'export',
 };
 
 export default nextConfig;
